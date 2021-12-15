f4c4f9291ec223cd1e77960827b7b86167fd6f58
Apu909 <apuapustaja191@gmail.com>
2021-12-15 17:26:08 +0200

    Comment code

--- script.js
+++ script.js
@@ -39,36 +39,50 @@ function loadImage(e) {
     image.onload = function () {
       let widthImage = image.width;
       let heightImage = image.height;
-
+      // daca imaginea este tip landscape (latimea este mai mare decat inaltimea)
       if (widthImage > heightImage) {
+        // daca imaginea depaseste latimea maxima a canvas-ului, va trebui sa o micsoram conform formulelor de mai jos
         if (widthImage > widthMax) {
           heightImage *= widthMax / widthImage;
           widthImage = widthMax;
         }
       } else {
+        // daca imaginea este de tip portret (inaltimea este mai mare decat latimea)
+        //daca inaltimea imaginii depaseste inaltimea maxima setata, imaginea va fi micsorata folosind formulele de mai jos
         if (heightImage > heightMax) {
           widthImage *= heightMax / heightImage;
           heightImage = heightMax;
         }
       }
+      // setam latimea si inaltimea canvas-ului cu latimea si inaltimea imaginii
       canvasImage.width = widthImage;
       canvasImage.height = heightImage;
+      // desenam imaginea (image) in canvas la coordonatele (0, 0), cu dimensiunea widthImage x heightImage
       ctx.drawImage(image, 0, 0, widthImage, heightImage);
 
+      // pentru aspect, atunci cand incarcam o imagine, vom ascunde input-ul cu care alegem imaginea si afisam canvas-ul ce contine imaginea selectata
+      // setam atributul "display" al canvas-ului cu imaginea cu valoarea "flex"
       canvasImage.style.display = "flex";
+      // ascundem input-ul pe care l-am folosit la alegerea si incarcarea imaginii
       imageLoaderInput.style.display = "none";
     };
     image.src = ev.target.result;
   };
   reader.readAsDataURL(e.target.files[0]);
+  // golim e.target.value deoarece aceasta functie este de tip "change"
+  // daca nu resetam valoarea event-ului, nu vom putea incarca aceeasi imagine de 2 ori consecutiv
   e.target.value = "";
 }
 
 imageLoaderInput.addEventListener("change", loadImage, false);
 
+// buton ce "sterge" imaginea curenta din canvas
 removeImageButton.addEventListener("click", () => {
-  selector.style.display = "none";
+  // ascunde selectorul (dreptunghiul generat dinamic - pentru selectie)
+  selectionRectangle.style.display = "none";
+  // ascunde canvas-ul cu imaginea
   canvasImage.style.display = "none";
+  // afiseaza input-ul pentru selectarea unei noi imagini
   imageLoaderInput.style.display = "flex";
 });
 
@@ -86,9 +100,10 @@ function downloadImage(data, name = "image.jpeg") {
   a.click();
 }
 
-//==================================================
+// formam un array ce contine toate butoanele din meniul principal
 const menuButtons = Array.from(document.querySelectorAll(".menuButton"));
 
+// verificam ce buton a fost apasat (in functie de id) si apelam functia corespunzatoare
 menuButtons.forEach((buttons) => {
   buttons.addEventListener("click", () => {
     switch (buttons.id) {
@@ -115,11 +130,12 @@ menuButtons.forEach((buttons) => {
 });
 
 // functia de selectie
-
 function selection() {
-  selector.style.display = "block";
+  // afisam div-ul pentru selectie
   selectionRectangle.style.display = "block";
+  // setam position = "absolute" pentru a putea muta dreptunghiul pentru selectie
   selectionRectangle.style.position = "absolute";
+  // initial, dreptunghiul pentru selectia va avea dimensiunea canvas-ului ce retine imaginea
   selectionRectangle.style.left = canvasImage.offsetLeft + "px";
   selectionRectangle.style.top = canvasImage.offsetTop + "px";
   selectionRectangle.style.width = canvasImage.width + "px";
@@ -130,7 +146,9 @@ function selection() {
   canvasImage.addEventListener("mouseup", mouseup);
   canvasImage.addEventListener("mousemove", mousemove);
 
+  // variabila hold este de tip boolean si este folosita pentru a stii daca utilizatorul inca tine butonul mouse-ului apasat
   let hold = false;
+  // rectangle retine coordonatele selectiei
   let rectangle = {
     x0: 0,
     y0: 0,
@@ -138,29 +156,38 @@ function selection() {
     y1: 0,
   };
 
+  // cand utilizatorul apasa click, variabila de hold devine true (butonul mouse-ului este inca apasat)
   function mousedown(e) {
     hold = true;
+    // coordonatele punctului din coltul sus-stanga va lua coordonatele click-ului initial
     rectangle.x0 = e.clientX;
     rectangle.y0 = e.clientY;
+    // coordonatele punctului unde a fost facut click sunt retinute in variabilele globale clickX si clickY pentru a fi folosite mai tarziu (in restul functiilor)
     clickX = e.layerX;
     clickY = e.layerY;
   }
 
+  // daca utilizatorul inca tine butonul mouse-ului apasat, coordonatele punctului din coltul jos-dreapta al dreptunghiului de selectie
+  // se vor modifica odata cu modificarea coordonatelor cursorului
   function mousemove(e) {
     if (hold) {
       rectangle.x1 = e.clientX;
       rectangle.y1 = e.clientY;
+      // in tot acest timp, afisam dreptunghiul de selectie
       showRect();
     }
   }
 
+  // cand ridicam degetul de pe butonul mouse-ului, variabila hold va deveni false si dreptunghiul de selectie va ramane la ultimele dimensiuni inregistrate
   function mouseup(e) {
     hold = false;
   }
 
   function showRect() {
+    // afisam dreptunghiul de selectie
     selectionRectangle.style.display = "block";
     selectionRectangle.style.position = "absolute";
+    // calculam dimensiunile acestuia in functie de coordonatele calculate
     selectionRectangle.style.left = rectangle.x0 + "px";
     selectionRectangle.style.top = rectangle.y0 + "px";
     selectionRectangle.style.width = rectangle.x1 - rectangle.x0 + "px";
@@ -168,11 +195,13 @@ function selection() {
   }
 }
 
-//functia de crop interactiv
+// functia de crop interactiv folosind dimensiunuile dreptunghiului de selectie
 function crop() {
-  const image = new Image();
-  image.src = canvasImage.toDataURL();
-
+  // redesenam imaginea in canvas
+  // clickX si clickY reprezinta coordonatele punctului unde a fost facut click initial (coltul stanga-sus al dreptunghiului de selectie)
+  // selectionRectangle.offsetWidth si selectionRectangle.offsetHeight reprezinta dimensiunea dreptunghiului de selectie (ce arie din poza sa fie taiata)
+  // 0 si 0 reprezinta punctele coordonatei din stanga-sus de unde va fi desenata imaginea
+  // canvasImage.width si canvasImage.height reprezinta dimensiunile in care va fi desenata noua imagine (cea caruia i-a fost aplicata efectul de crop)
   ctx.drawImage(
     canvasImage,
     clickX,
@@ -186,8 +215,9 @@ function crop() {
   );
 }
 
-//stergere pixeli
+// functie de stergere pixeli
 function cut() {
+  // preluam intr-o constanta datele imagini cu suprafata egala cu cea a dreptunghiului de selectie
   const imageSectionData = ctx.getImageData(
     clickX,
     clickY,
@@ -195,9 +225,11 @@ function cut() {
     selectionRectangle.offsetHeight
   );
   const imageData = imageSectionData.data;
+  // parcurgem zona selectata si setam pixelii zonei la valoarea 255 (alb)
   for (let i = 0; i < imageData.length; i++) {
     imageData[i] = 255; // alb
   }
+  // setam portiunea selectata din imagine cu datele modificate (alb)
   ctx.putImageData(
     imageSectionData,
     clickX,
@@ -341,6 +373,6 @@ addTextButton.addEventListener("click", () => {
   ctx.fillStyle = colorTextInput.value;
   // setam dimensiunea si fontul de baza al textului
   ctx.font = fontSize.value + "px Arial";
-  // la final, adaugam textul la contextul canvas-ului
+  // la final, adaugam textul in canvas
   ctx.fillText(text, clickX, clickY);
 });
